bc837fbda20a7cb2c8dddcf746b0dadf143a3cde
viniciosragazzidev <devviniciosragazzi@gmail.com>
2023-02-17 02:29:22 -0300

    Commit

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -30,14 +30,19 @@ export default function Navbar() {
         VR
       </a>
       <div
-        className={`menu w-full flex items-center max-[768px]:justify-center max-[768px]:gap-16 max-[768px]:text-center dark:text-primary-dark
+        className={`menu w-full flex z-50 items-center max-[768px]:justify-center max-[768px]:gap-16 max-[768px]:text-center dark:text-primary-dark
         text-primary-light max-[768px]:fixed max-[768px]:flex-col
          max-[768px]:h-screen  max-[768px]:left-0 max-[768px]:bg-bg-dark max-[768px]:top-[-100vh] transition-transform ${
            open ? "max-md:translate-y-[100vh]" : "max-md:translate-y-0"
          }`}
       >
         <span className="hidden max-md:flex absolute top-0 right-0 m-6">
-          <CloseIcon className=" cursor-pointer" onClick={()=>{setOpen(false)}} />
+          <CloseIcon
+            className=" cursor-pointer"
+            onClick={() => {
+              setOpen(false);
+            }}
+          />
         </span>
         <ul className="flex justify-center md:flex-1 gap-10 text-sm max-[768px]:flex-col max-md:text-primary-dark">
           <li className="hover:scale-95 transition-all">
